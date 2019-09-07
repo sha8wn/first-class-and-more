bae63823edc0c002e35150a8b5e569a26e6cbd2f
shawn frank <shawn@mindhyve.com>
2019-09-07 09:19:36 +0400

    - updated version and build number

--- First Class And More/AppDelegate.swift
+++ First Class And More/AppDelegate.swift
@@ -55,32 +55,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
             case invalidResponse, invalidBundleInfo
         }
 
-        func isUpdateAvailable(completion: @escaping (Bool?, Error?) -> Void) throws -> URLSessionDataTask {
-            guard let info = Bundle.main.infoDictionary,
-                let currentVersion = info["CFBundleShortVersionString"] as? String,
-                let identifier = info["CFBundleIdentifier"] as? String,
-                let url = URL(string: "http://itunes.apple.com/lookup?bundleId=\(identifier)") else {
-                    throw VersionError.invalidBundleInfo
-            }
-            print("current version", currentVersion)
-            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-                do {
-                    if let error = error { throw error }
-                    guard let data = data else { throw VersionError.invalidResponse }
-                    let json = try JSONSerialization.jsonObject(with: data, options: [.allowFragments]) as? [String: Any]
-                    guard let result = (json?["results"] as? [Any])?.first as? [String: Any], let version = result["version"] as? String else {
-                        throw VersionError.invalidResponse
-                    }
-                    completion(version != currentVersion, nil)
-                } catch {
-                    completion(nil, error)
-                }
-            }
-            task.resume()
-            return task
+    func isUpdateAvailable(completion: @escaping (Bool?, Error?) -> Void) throws -> URLSessionDataTask {
+        guard let info = Bundle.main.infoDictionary,
+            let currentVersion = info["CFBundleShortVersionString"] as? String,
+            let identifier = info["CFBundleIdentifier"] as? String,
+            let url = URL(string: "http://itunes.apple.com/lookup?bundleId=\(identifier)") else {
+                throw VersionError.invalidBundleInfo
         }
+        print("current version", currentVersion)
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            do {
+                if let error = error { throw error }
+                guard let data = data else { throw VersionError.invalidResponse }
+                let json = try JSONSerialization.jsonObject(with: data, options: [.allowFragments]) as? [String: Any]
+                guard let result = (json?["results"] as? [Any])?.first as? [String: Any], let version = result["version"] as? String else {
+                    throw VersionError.invalidResponse
+                }
+                completion(version != currentVersion, nil)
+            } catch {
+                completion(nil, error)
+            }
+        }
+        task.resume()
+        return task
+    }
 
-        _ = try? isUpdateAvailable { (update, error) in
+    _ = try? isUpdateAvailable { (update, error) in
             DispatchQueue.main.async {
                 if let error = error {
                     print(error)
